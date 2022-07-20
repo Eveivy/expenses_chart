2ae746ece058de73c1fcc50d083479fd962893a5
Isaac Evelyn <isaacevelyn93@gmail.com>
2022-07-20 15:54:25 +0100

    styled expenses chart

--- script/chart.js
+++ script/chart.js
@@ -1,22 +1,76 @@
-const labels = ['Mon', 'Tue', 'Wed', 'Thurs', 'Fri', 'Sat', 'Sun'];
-
-const data = {
-    labels: labels,
-    datasets: [{
-        label: 'spent',
-        backgroundColor: ['rgb(255, 99, 132)'],
-        borderColor: 'rgb(255, 99, 132)',
-        data: [0, 10, 5, 2, 20, 30, 45],
-    }]
-};
 
 const config = {
     type: 'bar',
-    data: data,
-    options: {}
+    options: {
+        scales: {
+            y: {
+                display: false
+            },
+            x: {
+                grid: {
+                    display: false,
+                    drawBorder: false
+                }
+            }
+        },
+        plugins: {
+            legend: {
+                display: false
+            },
+            tooltip: {
+                enabled: true,
+                displayColors: false,
+                yAlign: top, 
+                backgroundColor: 'hsl(25, 47%, 15%)',
+                callbacks: {
+                    title: function () { },
+                    label: function (context) {
+                        let label = context.dataset.label || '';
+
+                        if (label) {
+                            label += ': ';
+                        }
+                        if (context.parsed.y !== null) {
+                            label += new Intl.NumberFormat('en-US', {
+                                style: 'currency',
+                                currency: 'USD'
+                            }).format(context.parsed.y);
+                        }
+                        return label;
+                    }
+                }
+            }
+        }
+    },
+    data: {
+        labels: ['Mon', 'Tue', 'Wed', 'Thurs', 'Fri', 'Sat', 'Sun'],
+        datasets: [{
+            backgroundColor: ['hsl(10, 79%, 65%)',
+                'hsl(10, 79%, 65%)',
+                'hsl(186, 34%, 60%)',
+                'hsl(10, 79%, 65%)',
+                'hsl(10, 79%, 65%)',
+                'hsl(10, 79%, 65%)',
+                'hsl(10, 79%, 65%)'],
+            hoverBackgroundColor: ['hsla(10, 79%, 65%, 0.849)',
+             'hsla(10, 79%, 65%, 0.849)', 
+             'hsl(176, 34%, 60%)', 
+             'hsla(10, 79%, 65%, 0.849)', 
+             'hsla(10, 79%, 65%, 0.849)', 
+             'hsla(10, 79%, 65%, 0.849)', 
+             'hsla(10, 79%, 65%, 0.849)'],
+            borderRadius: 5,
+            data: [17.45, 34.91, 52.36, 31.07, 23.39, 43.28, 25.48],
+
+        }]
+    },
+
 };
 
 const myChart = new Chart(
     document.getElementById('myChart'),
     config
 );
+
+
+
